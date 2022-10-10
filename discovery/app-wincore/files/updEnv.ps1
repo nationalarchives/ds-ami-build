1c38e533cfc4dcb80428a4b99fb37900f221f317
Karl Kern <karl.kern@nationalarchives.gov.uk>
2022-10-10 08:37:12 +0100

    add read secrets manager values to api

--- discovery/app-wincore/files/updEnv.ps1
+++ discovery/app-wincore/files/updEnv.ps1
@@ -12,28 +12,7 @@ function write-log
    Add-content $logFile -value "$Time - $Severity - $Message"
 }
 
-#function retrieve-security-creds
-#{
-#    $sts = aws sts assume-role --role-arn arn:aws:iam::500447081210:role/discovery-s3-deployment-source-access --role-session-name s3-access | ConvertFrom-Json
-#    $Env:AWS_ACCESS_KEY_ID = $sts.Credentials.AccessKeyId
-#    $Env:AWS_SECRET_ACCESS_KEY = $sts.Credentials.SecretAccessKey
-#    $Env:AWS_SESSION_TOKEN = $sts.Credentials.SessionToken
-#    $Env:AWS_ACCESS_EXPIRATION = $sts.Credentials.Expiration
-#}
-
 try {
-#    if (Test-Path "env:\AWS_ACCESS_KEY_ID") {
-#        $now = Get-Date
-#        $endToken = Get-Date $Env:AWS_ACCESS_EXPIRATION
-#        if ($now -gt $endToken) {
-#            write-log -Message "security token renewal" -Severity "Information"
-#            retrieve-security-creds
-#        }
-#    } else {
-#        write-log -Message "security token requested" -Severity "Information"
-#        retrieve-security-creds
-#    }
-
     $sysEnv = $Env:TNA_APP_ENVIRONMENT
     $sysTier = $Env:TNA_APP_TIER
 
@@ -60,6 +39,31 @@ try {
         $envValue = $smValues."$smKey"
         write-log -Message "set: $smKey - $envValue" -Severity "Information"
         [System.Environment]::SetEnvironmentVariable($smKey.trim(), $envValue.trim(), "Machine")
+    }
+
+    if ($sysTier -eq "api")
+    {
+        write-log -Message "read secret arn"
+        $arnData = aws ssm get-parameter --name /devops/deployment/discovery.environment.mongodb.secrets-arn --region eu-west-2 | ConvertFrom-Json
+        $secrets_arn = $arnData.Parameter.Value | ConvertFrom-Json
+
+        write-log -Message "read environment variables from secrets manager"
+        $mongoSecrets = aws secretsmanager get-secret-value --secret-id $secrets_arn | ConvertFrom-Json
+        $userList = $mongoSecrets.SecretString | ConvertFrom-Json
+        foreach ($line in $userList.PSObject.Properties)
+        {
+            if ($line.Name -Match "^DISC_MONGO_")
+            {
+                $envVarNameUser = $line.Value.username + "_USR"
+                $envVarNamePassword = $line.Value.username + "_PWD"
+
+                write-log -Message "set: $envVarNameUser - $line.Value.username" -Severity "Information"
+                [System.Environment]::SetEnvironmentVariable($envVarNameUser.trim(),$line.Value.username.trim(), "Machine")
+
+                write-log -Message "set: $envVarNamePassword - $line.Value.password" -Severity "Information"
+                [System.Environment]::SetEnvironmentVariable($envVarNamePassword.trim(),$line.Value.password.trim(), "Machine")
+            }
+        }
     }
 
     net start w3svc
