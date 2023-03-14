1eb7c4557cf4603bfbeaee92cc7a47c455bd3a3b
karlkern <karl.kern@nationalarchives.gov.uk>
2023-03-14 12:33:45 +0000

    change s3 location

--- discovery/taxonomy-updater/files/server-setup.ps1
+++ discovery/taxonomy-updater/files/server-setup.ps1
@@ -18,7 +18,7 @@ $installerPackageUrl =  "s3://ds-intersite-deployment/discovery/installation-pac
 $cloudwatchAgentJSON = "discovery-cloudwatch-agent.json"
 $pathAWScli = "C:\Program Files\Amazon\AWSCLIV2"
 $dotnetSDK6 = "https://download.visualstudio.microsoft.com/download/pr/4a725ea4-cd2c-4383-9b63-263156d5f042/d973777b32563272b85617105a06d272/dotnet-sdk-6.0.406-win-x64.exe"
-$cloudwatchAgentInstaller = "https://s3.eu-west-1.amazonaws.com/amazoncloudwatch-agent-eu-west-1/windows/amd64/latest/amazon-cloudwatch-agent.msi"
+$cloudwatchAgentInstaller = "https://s3.eu-west-2.amazonaws.com/amazoncloudwatch-agent-eu-west-2/windows/amd64/latest/amazon-cloudwatch-agent.msi"
 $codeTarget = "c://Elastic-Taxonomy//batch-update"
 
 "=================> start server setup script" | Out-File -FilePath /debug.txt -Append
@@ -51,11 +51,11 @@ try {
     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
     Set-Service -Name WMSVC -StartupType Automatic
 
-    "===> aquire AWS credentials" | Out-File -FilePath /debug.txt -Append
-    $sts = Invoke-Expression -Command "aws sts assume-role --role-arn arn:aws:iam::500447081210:role/discovery-s3-deployment-source-access --role-session-name s3-access" | ConvertFrom-Json
-    $Env:AWS_ACCESS_KEY_ID = $sts.Credentials.AccessKeyId
-    $Env:AWS_SECRET_ACCESS_KEY = $sts.Credentials.SecretAccessKey
-    $Env:AWS_SESSION_TOKEN = $sts.Credentials.SessionToken
+##    "===> aquire AWS credentials" | Out-File -FilePath /debug.txt -Append
+##    $sts = Invoke-Expression -Command "aws sts assume-role --role-arn arn:aws:iam::500447081210:role/discovery-s3-deployment-source-access --role-session-name s3-access" | ConvertFrom-Json
+##    $Env:AWS_ACCESS_KEY_ID = $sts.Credentials.AccessKeyId
+##    $Env:AWS_SECRET_ACCESS_KEY = $sts.Credentials.SecretAccessKey
+##    $Env:AWS_SESSION_TOKEN = $sts.Credentials.SessionToken
 
     "===> download and install required packages and config files" | Out-File -FilePath /debug.txt -Append
     Set-Location -Path $tmpDir
